6b594fdac911cb1d115ab8f4013a15770295f26e
Shadi Abumattar <shadiabumattar@live.com>
2021-07-09 21:56:01 +0100

    Adds navigation menu and prerequisites for styling and animations

---
+++ new/assets/js/main.js
@@ -0,0 +1,26 @@
+/* Menu */
+
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+function clickOnNavLink() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+}
+
+navLink.forEach((n) => n.addEventListener("click", clickOnNavLink));
