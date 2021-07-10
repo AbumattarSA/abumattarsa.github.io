1429ec7127d46a131835534f5601bcceff7a52fa
Shadi Abumattar <shadiabumattar@live.com>
2021-07-10 11:01:13 +0100

    Adds content, styling and animations for skills section

--- new/assets/js/main.js
+++ new/assets/js/main.js
@@ -24,3 +24,24 @@ function clickOnNavLink() {
 }
 
 navLink.forEach((n) => n.addEventListener("click", clickOnNavLink));
+
+/* Skills */
+
+const skillsContent = document.getElementsByClassName("skills-content"),
+  skillsHeader = document.querySelectorAll(".skills-header");
+
+function skillsToggle() {
+  let itemClass = this.parentNode.className;
+
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills-content skills-close";
+  }
+
+  if (itemClass === "skills-content skills-close") {
+    this.parentNode.className = "skills-content skills-open";
+  }
+}
+
+skillsHeader.forEach((el) => {
+  el.addEventListener("click", skillsToggle);
+});
