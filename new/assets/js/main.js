46af301134ff0d1a95c4f4979cf01304d7b9279e
Shadi Abumattar <shadiabumattar@live.com>
2021-07-10 18:31:43 +0100

    Creates custom scrollbar and adds scroll to top button

--- new/assets/js/main.js
+++ new/assets/js/main.js
@@ -110,3 +110,43 @@ let swiper = new Swiper(".portfolio-container", {
   mousewheel: true,
   keyboard: true,
 });
+
+/* Scrolling */
+
+const sections = document.querySelectorAll("section[id]");
+
+function activeScroll() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav-menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav-menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+
+window.addEventListener("scroll", activeScroll);
+
+function scrollHeader() {
+  const nav = document.getElementById("header");
+  if (this.scrollY >= 80) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
+
+function scrollToTop() {
+  const scrollToTop = document.getElementById("scroll-up");
+  if (this.scrollY >= 560) scrollToTop.classList.add("show-scroll");
+  else scrollToTop.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollToTop);
